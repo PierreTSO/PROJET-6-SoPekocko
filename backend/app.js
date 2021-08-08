ed97f68e50099d727e59ccda60a9245cb5929977
thrace92i <pierre.fournier42@gmail.com>
2021-08-08 12:04:58 +0200

    Installation body-parser

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
+
+app.use(bodyParser.json());
 
 module.exports = app;
