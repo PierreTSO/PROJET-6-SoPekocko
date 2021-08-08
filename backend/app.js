1e720641070221e0d9d0ea7bcee18d6e7a33120b
thrace92i <pierre.fournier42@gmail.com>
2021-08-08 12:31:16 +0200

    Connexion à MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://thraceSoPeko:2BSoTH8rJMDMs1DjRubx@cluster0.h5ecw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //Header permettant d'accéder à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //d'envoyer des requêtes avec les méthodes mentionnées
+    next();
+  });
+
 app.use(bodyParser.json());
 
 module.exports = app;
+
